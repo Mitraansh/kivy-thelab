95b8212137d6a82f0d85c099cff680ead48844ba
Mitraansh <mitransh30@gmail.com>
2021-07-18 15:02:32 +0530

    Add Toggle Button

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from kivy.app import App
 from kivy.metrics import dp
+from kivy.properties import StringProperty
 from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
@@ -8,17 +9,39 @@ from kivy.uix.stacklayout import StackLayout
 from kivy.uix.widget import Widget
 
 
+class WidgetsExample(GridLayout):
+    count = 1
+    count_enabled = False
+    #my_text = StringProperty("Hello!")
+    my_text = StringProperty("1")
+
+    def on_button_click(self):
+        print("Button clicked")
+        if self.count_enabled:
+        #self.my_text = "You Clicked!"
+            self.count += 1
+            self.my_text = f"{self.count}"
+
+    def on_toggle_button_state(self, widget):
+        print("Toggle State: " + widget.state)
+        if widget.state == "normal":
+            widget.text = "OFF"
+            self.count_enabled = False
+        else:
+            widget.text = "ON"
+            self.count_enabled =True
+
 class StackLayoutExample(StackLayout):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        #self.orientation = "lr-tb"
+        # self.orientation = "lr-tb"
         for i in range(0, 100):
-            #size = dp(100) + i*10
+            # size = dp(100) + i*10
             size = dp(100)
-            #b = Button(text=str(i+1), size_hint=(.2, .2))
-            #self.add_widget(b)
-            #b = Button(text=str(i + 1), size_hint=(None, None), size=(dp(100), dp(100)))
-            #self.add_widget(b)
+            # b = Button(text=str(i+1), size_hint=(.2, .2))
+            # self.add_widget(b)
+            # b = Button(text=str(i + 1), size_hint=(None, None), size=(dp(100), dp(100)))
+            # self.add_widget(b)
             b = Button(text=str(i + 1), size_hint=(None, None), size=(size, size))
             self.add_widget(b)
 
